33dd645ba073edd9655a0b9ee6aca5e7d258f8ef
Agustin Camarero <aguscamarero@hotmail.com>
2020-03-08 21:10:40 -0300

    agrego faq

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/faq.css">
+    <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Work+Sans&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/86f3787a46.js" crossorigin="anonymous"></script>
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+    <header>
+        <?php require_once("header.php"); ?>
+    </header>
+      </div>
+      <div class="contenido">
+        <section id="content-all">
+          <section id="content-header">
+              <h2>Preguntas frecuentes</h2>
+          </section>
+            <div id="cont">
+              
+                  <h3>Menu:</h3>
+                    <ul>
+                        <li><a href="#">En que consiste el juego?</a></li>
+                        <li class="respuesta">"Es un juego de preguntas que provara tu ingenio y conocimiento sobre cultura general."</li>
+                        <li><a href="#">Como se suman puntos?</a></li>
+                        <li class="respuesta">"Contestando bien las respuestas"</liclass="respuesta">
+                        <li><a href="#">Como editar mi foto de perfil?</a></li>
+                        <li class="respuesta">"Ir a perfil y seleccionar cambiar foto."</liclass="respuesta">
+                        <li><a href="#">-Como crear usuario?</a></li>
+                        <li class="respuesta">"Ve a la pagina de <a href="#">Inicio.php</a> y selecciona crear usuario"</liclass="respuesta">
+                        <li><a href="#">Como llegar al primer puesto?</a></li>
+                        <li class="respuesta">"Respondiendo la mayor cantidad de preguntas correctas!"</liclass="respuesta">
+                    </ul>
+                </div>    
+        </section>
+      </div>
+      <div id="footer">
+      <footer>
+       <?php require_once("footer.php")?>
+       </footer>
+      </div>
+    </div>
+  </body>
+</html>
